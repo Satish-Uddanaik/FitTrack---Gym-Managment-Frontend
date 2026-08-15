8c3304c394e1e37735ac8e2e5c3d2964e05b4733
Satish Uddanaik <satishuddanaik@gmail.com>
2026-08-15 19:14:29 +0530

    remove the stash

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 
+
 const DashboardLayout = () => {
 
     return (
